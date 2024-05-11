2383afda31f69951ab7977d79fc1c8bc37002b8a
Siddhi Rungta <siddhi.rungta@gmail.com>
2024-05-11 21:31:15 +0530

    first commit

---
+++ KBC.C
@@ -0,0 +1,365 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <dos.h>
+
+int counter();
+int main()
+{
+long int i=0;
+int answer;
+printf("\t\t\t\t\n\n\t\t\t\tWELCOME TO\n\t\t\t KAUN BANEGA CROREPATI!!!!");
+printf("\n\n\n\n(press enter to continue...)");
+getchar();
+
+printf("\t\t\t\t\n\n\t\t\t\tLET'S CONTINUE...");
+printf("\n\n\n(press enter to continue...)");
+getchar();
+
+printf("1)Who was the first prime minister of india?\n");
+printf("\n1)P.L Shastri \t 2)Indira Gandhi\n");
+printf("\n3)Motilal Nehru\t 4)J.N Nehru\n");
+scanf("%d",&answer);
+if(answer==4)
+{
+
+printf("CORRECT!!!\n");
+i=i+1000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+
+getchar();
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+
+getchar();
+
+printf("2)Which is the highest mountain in the world?\n ");
+printf("\n1)Mt. Lhotse\t 2)Mt. Abu\n");
+printf("\n3)Mt. K2\t 4)Mt. Everest\n");
+scanf("%d",&answer);
+if(answer==4)
+{
+printf("CORRECT!!!\n");
+i=i+1000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+getchar();
+
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("3)What is the capital of Austrailia?\n ");
+printf("\n1)Sydney\t 2)Canberra\n");
+printf("\n3)Melbourne\t 4)Brisbane\n");
+scanf("%d",&answer);
+if(answer==2)
+{
+printf("CORRECT!!!\n");
+i=i+1000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+getchar();
+
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+
+
+getchar();
+
+printf("4) In what year did the Titanic sink?\n ");
+printf("\n1)1912\t 2)1920\n");
+printf("\n3)1822\t 4)1856\n");
+scanf("%d",&answer);
+if(answer==1)
+{
+printf("CORRECT!!!\n");
+i=i+2000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+getchar();
+
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("5)Which of these personalities was appointed the youngest ever\nUN messenger of peace in April 2017?\n ");
+printf("\n1)Malia Obama\t \t 2)Piyanka Chopra\n");
+printf("\n3)Malala Yousafzai \t 4)Aylan Kurdi\n");
+scanf("%d",&answer);
+if(answer==3)
+{
+printf("CORRECT!!!\n");
+i=i+5000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+getchar();
+
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("6)Which of these group of bones is also called\n'merudand'?\n");
+printf("\n1)Backbone\t 2)Skull\n");
+printf("\n3)Pelvis\t 4)Ribs\n");
+scanf("%d",&answer);
+if(answer==1)
+{
+printf("CORRECT!!!\n");
+i=i+10000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+getchar();
+
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("7)In mythology,Kubera,the lord of wealth,shared\nwhat relationship with Ravana?\n");
+printf("\n1)Cousin\t 2)Step-brother\n");
+printf("\n3)Father-in-Law\t 4)Uncle\n");
+scanf("%d",&answer);
+if(answer==2)
+{
+printf("CORRECT!!!\n");
+i=i+10000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("8)Which social media platform reached the milestone\nof 2 billion users in June 2017?\n");
+printf("\n1)Twitter\t 2)Instagram\n");
+printf("\n3)Snapchat\t 4)Facebook\n");
+scanf("%d",&answer);
+if(answer==4)
+{
+printf("CORRECT!!!\n");
+i=i+20000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("9)Who painted the famous artwork Starry Night?\n");
+printf("\n1)Vincent Van Gogh\t 2)Pablo Picasso\n");
+printf("\n3)Leonardo Da Vinci\t 4)Claude Monet\n");
+scanf("%d",&answer);
+if(answer==1)
+{
+printf("CORRECT!!!\n");
+i=i+50000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("10)Which planet is known as the Morning Star? \n");
+printf("\n1)Saturn\t 2)Venus\n");
+printf("\n3)Mars\t 4)Neptune\n");
+scanf("%d",&answer);
+if(answer==2)
+{
+printf("CORRECT!!!\n");
+i=i+200000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("11)Which of the following is not the name of a Galaxy?\n");
+printf("\n1)Andromeda\t 2)Milky Way\n");
+printf("\n3)Eledra\t 4)Tadpole\n");
+scanf("%d",&answer);
+if(answer==3)
+{
+printf("CORRECT!!!\n");
+i=i+200000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("12)Which of these sweets is specially prepared during\nChath Pooja and and offered as prasad?\n");
+printf("\n1)Kalakand\t 2)Thekuwa\n");
+printf("\n3)Thepla\t 4)Rabri\n");
+scanf("%d",&answer);
+if(answer==2)
+{
+printf("CORRECT!!!\n");
+i=i+500000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("13)Which of the following hormone is also known as the\n'Love Hormone'?\n");
+printf("\n1)Testerone\t 2)Progesterone\n");
+printf("\n3)Insulin\t 4)Oxytocin\n");
+scanf("%d",&answer);
+if(answer==4)
+{
+printf("CORRECT!!!\n");
+i=i+2000000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("14)In which field did Jeffery Hall,Michael Roshbash and\nMichael Young win the 2017 Nobel Prize?\n");
+printf("\n1)Physiology or Medicine\t 2)Physics\n");
+printf("\n3)Chemistry\t 4)Literature\n");
+scanf("%d",&answer);
+if(answer==1)
+{
+printf("CORRECT!!!\n");
+i=i+2000000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getchar();
+
+printf("15)In which Olympic games was the badminton introduced\nas a medal sport?\n");
+printf("\n1)Munich 1972\t\t 2)Seoul 1988\n");
+printf("\n3)Barcelona 1992\t 4)Atlanta 1996\n");
+scanf("%d",&answer);
+if(answer==3)
+{
+printf("CORRECT!!!\n");
+i=i+5000000;
+printf("You win rs.%ld\n",i);
+getchar();
+}
+else
+{
+printf("Wrong Answer!!\n");
+printf("you earned rs.%ld",i);
+printf("\n\n\nGAME OVER!!!\n");
+getchar();
+exit(0);
+getchar();
+}
+getch();
+}
